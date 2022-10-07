82ee15c596b564ab36d7b7a0ea2e920fd7a26085
Tess von Rueden <tessvonr@umich.edu>
2022-10-07 11:24:35 -0400

    rover mouseover

--- js/page.js
+++ js/page.js
@@ -1,4 +1,10 @@
 // Main
-function myFunction() {
-    document.getElementById("demo").innerHTML = "ryan sucks!";
+function showInfo() {
+    document.getElementById("roverbox").style.visibility = 'visible';
+    document.getElementById("rover").src = "images/BLiSS_Rover2.png";
+}
+
+function hideInfo() {
+    document.getElementById("roverbox").style.visibility = 'hidden';
+    document.getElementById("rover").src = "images/BLiSS_Rover.png";
 }
