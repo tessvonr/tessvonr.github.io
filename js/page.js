28f0d0f8e63beb51cc74b2e3a45a45a5278299e0
Tess von Rueden <tessvonr@umich.edu>
2023-04-05 16:51:54 -0400

    Converting to personal portfolio

--- js/page.js
+++
@@ -1,9 +0,0 @@
-// Main
-function showInfo() {
-    document.getElementById("rover").src = "images/BLiSS_Rover2.png";
-}
-
-function hideInfo() {
-    document.getElementById("roverbox").style.visibility = 'hidden';
-    document.getElementById("rover").src = "images/BLiSS_Rover.png";
-}
